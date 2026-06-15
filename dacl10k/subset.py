76cc4463c95caea025ce14f71201a9b449b93190
Helen Dawit <helendawit626@gmail.com>
2026-06-15 22:06:49 +0200

    Update subset.py

--- dacl10k/subset.py
+++ dacl10k/subset.py
@@ -2,7 +2,7 @@ import fiftyone as fo
 
 dataset = fo.load_dataset("dacl10k")
 
-subset = dataset.take(500, seed=42)
+subset = dataset.take(1000, seed=42)
 
 session = fo.launch_app(subset)
 session.wait()
