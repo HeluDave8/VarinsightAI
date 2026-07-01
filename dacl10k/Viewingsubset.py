1695360dbf07bd2a015696fe4590c790330ca489
Helen Dawit <helendawit626@gmail.com>
2026-07-01 22:25:01 +0200

    Update Viewingsubset.py

--- dacl10k/Viewingsubset.py
+++ dacl10k/Viewingsubset.py
@@ -2,7 +2,7 @@ import fiftyone as fo
 
 dataset = fo.load_dataset("dacl10k")
 
-view = dataset.load_saved_view("my_tagged_subset")
+view = dataset.load_saved_view("my_tagged_subset") # you can name it anything you want, this will be shown in fiftyone.The subset is saved in fiftyone as my_tagged_subset
 
 session = fo.launch_app(view)
 session.wait()
